3c7fddd2db5ffe9cbc105c3dd324f07611989fbe
yanbrasiliano <yanpenabr@gmail.com>
2021-01-31 12:38:55 -0300

    Create ytautomatic.py

---
+++ ytautomatic.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+
+# user typed #
+link = input("Enter the video link:  ")
+path = input("Enter the directory that the video will be saved:  ")
+yt = YouTube(link)
+
+# details#
+print(f"Title: {yt.title}.")
+print(f"Number of vies: {yt.views}.")
+print(f"Video duration {yt.length} seconds.")
+print(f"Rating: {yt.rating}.")
+
+# best resolution
+ys = yt.streams.get_highest_resolution()
+
+# download #
+print("Download in progress ...")
+ys.download(path)
+print("Download completed.\nCheck your repository and enjoying the video.")
