13dee2b7469c046a52e51273e5293138cf9e7c22
yanbrasiliano <yanpenabr@gmail.com>
2021-03-26 22:22:05 -0300

    Update ytautomatic.py

--- ytautomatic.py
+++ ytautomatic.py
@@ -6,17 +6,12 @@ Created by: Yan Brasiliano Silva Penalva
 Objective: Download automatic videos youtube.
 
 """
-
-print()
-print('Author: Yan Brasiliano Silva Penalva - hiyan')
-print()
-
-# user typed #
+# user typed 
 link = input("Enter the video link:  ")
 path = input("Enter the directory that the video will be saved:  ")
 yt = YouTube(link)
 
-# details#
+# details 
 print(f"Title: {yt.title}.")
 print(f"Number of vies: {yt.views}.")
 print(f"Video duration {yt.length} seconds.")
